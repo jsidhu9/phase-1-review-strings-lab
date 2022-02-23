0be01dc1c930891ef50927b5b451a42b890e9e65
+ <+>
2022-02-23 09:21:52 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this file
+const currentUser = "Jaz"
+const welcomeMessage = "Welcome to Flatbook, My name is Jaz!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, My name is JAZ!"
+const shortGreeting = "Welcome, Im J!"
